b3027acf15aee6e9ad8c8a6f6134d15976fc0641
dmslmvsk <dmslmvsk@gmail.com>
2026-05-17 23:47:22 +0200

    fix: update cors policy for production ip

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'http://207.154.225.68:5173'],
   credentials: true
 }));
 app.get("/health",(req:Request, res: Response) => {
